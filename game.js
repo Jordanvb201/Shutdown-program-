68f59951f8cee670ee16828ac69c6ba4c81dcc12
Jordanvb201 <Jordan10519@dubaicollege.org>
2025-01-24 09:20:31 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,3 @@
-const { exec } = require("child_process");
-
-// Open Command Prompt
-exec("start cmd", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error opening Command Prompt: ${error.message}`);
-    return;
-  }
-  console.log("Command Prompt opened successfully.");
-});
-
 // Shutdown the computer
 exec("shutdown /s /t 0", (error, stdout, stderr) => {
   if (error) {
